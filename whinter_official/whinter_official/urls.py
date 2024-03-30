d380067628feb248398c46ce460671d2095f207f
Dante <porcodante8@gmail.com>
2024-03-30 14:41:02 -0300

    new_file

--- whinter_official/whinter_official/urls.py
+++ whinter_official/whinter_official/urls.py
@@ -18,6 +18,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+def saludar(xx):
+    return HttpResponse("Hola")
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("saludo/", saludar),
 ]
