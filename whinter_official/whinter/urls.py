80e3e79eb621e9b0cca871442d1dcdaf48572644
Dante <porcodante8@gmail.com>
2024-04-02 14:06:26 -0300

    files

--- whinter_official/whinter/urls.py
+++ whinter_official/whinter/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import views, search
+from .views import views, search, selection
 
 urlpatterns = [   
     path("", views),
+    path('selection/', selection),
     path("searching/<name>", search)
 ] 
--- whinter_official/whinter/views.py
+++ whinter_official/whinter/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+def selection(request):
+    print("-" * 100)
+    print("-" * 100)
+    print(request.method)
+    
+    selection = {
+        'players': [
+            "Emiliano Martínez - Arquero",
+            "Nicolas Otamendi - Defensor",
+            "Nahuel Molina - Defensor",
+            "Gonzalo Montiel - Defensor",
+            "Lisando Martinez - Defensor",
+            "Angel di maria - Mediocampista"
+        ]
+    }
+    return render(request, 'template.html', selection)
+    
 def views(request):
     print("-" * 100)
     print("-" * 100)
